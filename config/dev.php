078df665e94995bf658301dfe1d19612b86c984c
Hery Ravelojaona <58849606+HeryRavelojaona@users.noreply.github.com>
2020-04-30 17:16:20 +0200

    Delete dev.php

--- config/dev.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// database connection
-const HOST = 'localhost';
-const DB_NAME = 'blog';//'rahe7740_blog';
-const CHARSET = 'utf8';
-const DB_HOST = 'mysql:host='.HOST.';dbname='.DB_NAME.';charset='.CHARSET;
-const DB_USER = 'root';//'rahe7740_hery';
-const DB_PASS = '';//'Hery454694*';
