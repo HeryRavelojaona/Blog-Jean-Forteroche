082bad0b51300e98408bf13e19cb204dc36a7e70
Hery Ravelojaona <hery.ravelojaona@gmail.com>
2020-05-02 13:43:13 +0200

    modif config dev

--- config/dev.php
+++ config/dev.php
@@ -8,8 +8,3 @@ const DB_HOST = 'mysql:host='.HOST.';dbname='.DB_NAME.';charset='.CHARSET;
 const DB_USER = 'root';
 const DB_PASS = '';
 
-// mailing
-const EMAIL_HOST = 'mail.heryravelojaona.fr';
-const EMAIL_PORT = 26;
-const EMAIL_USERNAME = 'contact@heryravelojaona.fr';
-const EMAIL_PASSWORD = 'Hery454694*';
